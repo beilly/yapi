946e58651253d53a1177d210029fdc71448c7f52
beilly <shibenli@mmfintec.com>
2020-12-24 17:44:54 +0800

    chore: 添加配置信息

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "port": process.env.PORT,
+  "adminAccount": "admin@admin.com",
+  "timeout":120000,
+  "db": {
+    "connectString": process.env.CONNECTSTRING,
+    "servername": "127.0.0.1",
+    "DATABASE": "yapi",
+    "port": 27017,
+    "user": process.env.CONNECTUSER,
+    "pass": process.env.CONNECTPASS,
+    "authSource": "yapi"
+  },
+  "mail": {
+    "enable": true,
+    "host": "smtp.163.com",
+    "port": 465,
+    "from": "***@163.com",
+    "auth": {
+      "user": "***@163.com",
+      "pass": "*****"
+    }
+  }
+}
